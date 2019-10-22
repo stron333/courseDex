c88d4e7c53719fa17f424f271d87e0234ec09f78
stron333 <stron333@list.ru>
2019-10-22 21:55:38 +0300

    Первое

---
+++ ConsoleApp1/Phone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Phone
+    {
+        public string Model { get; set; }
+        public string Price { get; set; }
+
+        public void Call(string number)
+        {
+            Dialing(number);
+        }
+
+        private void Dialing(string number) { }
+    }
+
+    public class PhoneWithCamera : Phone
+    {
+        public void GetPhoto()
+        {
+            var capture = GetCapture();
+            SavePhoto(capture);
+        }
+        private Photo GetCapture()
+        {
+            return new Photo();
+        }
+        private void SavePhoto(object capture){ }
+    }
+
+    public class SmartPhone : PhoneWithCamera
+    {
+
+    }
+
+    public class Photo
+    {
+        public string Name { get; set; }
+        public int Size
+        {
+            get { return Height * Weight; }
+        }
+
+        public int Height { get; set; }
+        public int Weight { get; set; }
+        public Photo()
+        {
+            Name = "Photo("+DateTime.Now.ToString()+")";
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneWithCamera myPhone = new PhoneWithCamera();
+            myPhone.Call("456484564");
+            myPhone.GetPhoto();
+            Photo ph = new Photo();
+            ph.Name = "gdfgd";
+            ph. = 54;
+        }
+    }
+}
