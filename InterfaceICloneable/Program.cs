a37ef881d53e3e2ff3e60a118fba1fc1f35edbe9
Александр Рыбарук <stron333@ro.ru>
2019-11-28 09:55:01 +0200

    ICloneable

--- InterfaceICloneable/Program.cs
+++ InterfaceICloneable/Program.cs
@@ -24,7 +24,7 @@ namespace InterfaceICloneable
         }
     }
 
-    struct MyStruct: objtICloneable
+    struct MyStruct: ICloneable
     {
         public ValueType value1;
         public ValueType value2;
