bc22598a77d72db7464523de0854ee9344b6a5a2
stron333 <stron333@list.ru>
2020-04-28 15:46:28 +0300

    создал класс с прямоугольниками

--- TestIComparable/Program.cs
+++ TestIComparable/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TestIComparable
 {
@@ -6,7 +7,18 @@ namespace TestIComparable
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rnd = new Random();
+            List<SomeFigure> someFigures = new List<SomeFigure>();
+            for (int i = 0; i<10; i++)
+            {
+                someFigures.Add(new SomeFigure(rnd.Next(1, 10), rnd.Next(1, 10)));
+            }
+
+            foreach (var someFigure in someFigures)
+            {
+                Console.WriteLine("a = {0} b = {1} area = {2}",someFigure.A, someFigure.B, someFigure.Area);
+            }
+            Console.ReadLine();
         }
     }
 }
--- TestIComparable/SomeFigure.cs
+++ TestIComparable/SomeFigure.cs
@@ -7,10 +7,55 @@ namespace TestIComparable
 {
     class SomeFigure: IComparable<int>
     {
-        public int A { get; set; }
-        public int B { get; set; }
-        public int Area { get; set; }
+        private int a;
+        private int b;
+        private int area;
+        public int A
+        {
+            get
+            {
+                return a;
+            }
 
+            set
+            {
+                a = value;
+                CountArea();
+            }
+        }
+        public int B
+        {
+            get
+            {
+                return b;
+            }
+
+            set
+            {
+                b = value;
+                CountArea();
+            }
+        }        
+
+        public int Area
+        {
+            get
+            {
+                return area;
+            }                        
+        }
+
+        public SomeFigure (int _a, int _b)
+        {
+            a = _a;
+            b = _b;
+            CountArea();
+        }
+
+        private void CountArea()
+        {
+            area = a * b;
+        }
         public int CompareTo([AllowNull] int other)
         {
             return Area.CompareTo(other);
