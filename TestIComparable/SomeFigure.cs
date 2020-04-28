b2a58011a717a004639f747161879c2e9135a0b4
stron333 <stron333@list.ru>
2020-04-28 11:46:30 +0300

    ставлю Microsoft Code Analysis

---
+++ TestIComparable/SomeFigure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace TestIComparable
+{
+    class SomeFigure: IComparable<int>
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+        public int Area { get; set; }
+
+        public int CompareTo([AllowNull] int other)
+        {
+            return Area.CompareTo(other);
+        }
+    }
+}
