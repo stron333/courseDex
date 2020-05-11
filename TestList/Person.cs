c9db7a81ace1f58114ed29b8570380c1bcfe43a0
stron333 <stron333@list.ru>
2020-05-11 12:49:40 +0300

    начало list

---
+++ TestList/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestList
+{
+    class Person
+    {
+        public string FIO { get; set; }
+        public DateTime DataRogdenia { get; set; }
+        public string MestoRogdenia { get; set; }
+        public int NomPasporta { get; set; }
+        
+        public Person(string FIO, DateTime DataRogdenia, string MestoRogdenia, int NomPasporta)
+        {
+            this.FIO = FIO;
+            this.DataRogdenia = DataRogdenia;
+            this.MestoRogdenia = MestoRogdenia;
+            this.NomPasporta = NomPasporta;
+        }        
+    }
+}
---
+++ TestList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
